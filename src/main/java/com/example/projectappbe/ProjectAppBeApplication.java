886678e963e8c37ddb7cc53344527fad164769f8
Prasad <balujustin@gmail.com>
2025-01-16 09:23:12 +0530

    initial commit

---
+++ src/main/java/com/example/projectappbe/ProjectAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAppBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAppBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectappbe/ProjectAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
